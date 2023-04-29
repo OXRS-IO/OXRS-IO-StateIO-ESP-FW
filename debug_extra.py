a0a5499f04454c4510a616342d479d9256329151
moinmoin-sh <53935853+moinmoin-sh@users.noreply.github.com>
2023-04-29 23:01:02 +0200

    changed namings

--- debug_extra.py
+++ debug_extra.py
@@ -7,9 +7,9 @@ config = env.GetProjectConfig()
 # get the env name for this build and check if we are building for 8266
 env_name = env.subst("$PIOENV")
 if "8266" in env_name:
-  firmware_name_raw = config.get("firmware", "name_room8266")
+  firmware_name_raw = config.get("firmware", "name_esp8266")
 else:
-  firmware_name_raw = config.get("firmware", "name_rack32")
+  firmware_name_raw = config.get("firmware", "name_esp32")
 
 firmware_name = firmware_name_raw.replace('\\\"', '')
 print("Firmware Name: %s" % firmware_name)
